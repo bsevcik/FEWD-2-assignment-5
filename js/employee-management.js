abaf247f023fa627e29660b17d07b03b15642484
Ben Sevcik <43259346+bsevcik@users.noreply.github.com>
2019-04-04 17:10:51 -0700

    updated id's, need to fix delete buttons

--- js/employee-management.js
+++ js/employee-management.js
@@ -1,101 +1,97 @@
 /*eslint-env browser*/
 var command;
 var employeeList = [
-        "Zak Ruvalcaba",
-        "Sally Smith",
-        "Fred Franklin",
-        "John Smith",
-        "Jane Caruthers"];
+        ["Zak Ruvalcaba", "Instructor", "1", "<input onclick='deleteEmployee()' type='button' class='tableButton' value='Delete'>"],
+        ["Sally Smith", "Student", "2", "<input onclick='deleteEmployee()' type='button' class='tableButton' value='Delete'>"],
+        ["Fred Franklin", "Principle", "3", "<input onclick='deleteEmployee()' type='button' class='tableButton' value='Delete'>"],
+        ["John Smith", "Student", "4", "<input onclick='deleteEmployee()' type='button' class='tableButton' value='Delete'>"],
+        ["Jane Caruthers", "Receptionist", "5", "<input onclick='deleteEmployee()' type='button' class='tableButton' value='Delete'>"]
+    ]
 
-document.getElementById("checkIn").addEventListener("click",
+window.addEventListener("load", showEmployees);
+        
+// This function is meant to clear the HTML table after the form is submitted. It wasn't reliable when using tableRow.length, so I set it to delete as many items as are in the array. The form data is pushed to the array before clearTable() is run
+function clearTable() {
+    "use strict";
+    for (var i = 0; i < employeeList.length; i++)
+        document.getElementById("checkedIn").deleteRow(-1);
+}
+
+document.getElementById("addEmployee").addEventListener("click",
     function update() {
         "use strict";
-        var time = new Date();
-        var timeNow = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
         if (document.getElementById("nameTyped").value === "" || document.getElementById("titleTyped").value === "" || document.getElementById("extensionTyped").value === "") {
             window.alert("You need to fill out all three forms");
         } else {
             var name = document.getElementById("nameTyped").value;
             var nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
-            employeeList.push([nameCapitalized, document.getElementById("titleTyped").value, document.getElementById("extensionTyped").value, "<input onclick='deleteEmployee(this)' type='button' class='tableButton' value='Return'>"]);
+            employeeList.push([nameCapitalized, document.getElementById("titleTyped").value, document.getElementById("extensionTyped").value, "<input onclick='deleteEmployee(this)' type='button' class='tableButton' value='Delete'>"]);
 //            clearTable();
             showEmployees();
-//    optionally call the function to export data to Csv. It was annoying so I turned it off.
-//            exportToCsv();
-//            window.console.log(checkedInArray);
-
-
-//This clears the written input forms after the information is written to checkedInArray and focuses back on nameTyped at the top of the form
+//            window.console.log(employeeList);
+//This clears the written input forms after the information is written to employeeList and focuses back on nameTyped at the top of the form
             document.getElementById("nameTyped").value = "";
             document.getElementById("titleTyped").value = "";
             document.getElementById("extensionTyped").value = "";
-//            document.getElementById("nameTyped").focus();
+//             document.getElementById("nameTyped").focus();
         }
-
     }
 );
 
 
-var employeeList = [
-        "Zak Ruvalcaba",
-        "Sally Smith",
-        "Fred Franklin",
-        "John Smith",
-        "Jane Caruthers"];
-
+function deleteEmployee(arg) {
+    "use strict";
+    // arg.
+    // this.innerHTML = "";
+    console.log(arg);
+}
 
 function showEmployees() {
     "use strict";
     clearTable();
-    for (var i = 0; i < checkedInArray.length; i++) {
-        document.getElementById("checkedIn").innerHTML += "<tr><td>" + checkedInArray[i][0] + "</td> <td>" + checkedInArray[i][1] + "</td> <td>" + checkedInArray[i][2] + "</td><td>" + checkedInArray[i][3] + "</td><td>" + checkedInArray[i][4] + "</td></tr>";
+    for (var i = 0; i < employeeList.length; i++) {
+        document.getElementById("checkedIn").innerHTML += "<tr><td>" + employeeList[i][0] + "</td> <td>" + employeeList[i][1] + "</td> <td>" + employeeList[i][2] + "</td><td>" + employeeList[i][3] + "</td></tr>";
     }
 }
-function addEmployees(arg) {
-    "use strict";
-    var employee = window.prompt("Enter the employee\'s name");
-    arg.push(employee);
-    window.console.log(employee + " was added.\n");
-}
-function deleteEmployees(arg) {
-    "use strict";
-    var num, employee;
-    num = parseInt(window.prompt("Employee number to delete"), 10);
-    if (num < 1 || num > arg.length) {
-        window.alert("Invalid employee number");
-    } else {
-        delete arg[num - 1];
-    }
-}
-function main() {
-    "use strict";
-//    var employeeList, command;
-//    displayMenu();
+// function addEmployees(arg) {
+//     "use strict";
+//     var employee = window.prompt("Enter the employee\'s name");
+//     arg.push(employee);
+//     window.console.log(employee + " was added.\n");
+// }
+// function deleteEmployee(arg) {
+//     "use strict";
+// console.log(arg);
+// }
+// function main() {
+//     "use strict";
+// //    var employeeList, command;
+// //    displayMenu();
     
-    employeeList = [
-        "Zak Ruvalcaba",
-        "Sally Smith",
-        "Fred Franklin",
-        "John Smith",
-        "Jane Caruthers"];
+//     employeeList = [
+//         "Zak Ruvalcaba",
+//         "Sally Smith",
+//         "Fred Franklin",
+//         "John Smith",
+//         "Jane Caruthers"];
     
-//    while (true) {
-//        command = window.prompt("Enter command");
+// //    while (true) {
+// //        command = window.prompt("Enter command");
     
-        if (command !== null) {
-            if (command == "show") {
-                showEmployees(employeeList);
-            } else if (command == "add") {
-                addEmployees(employeeList);
-            } else if (command == "del") {
-                deleteEmployees(employeeList);
-//            } else if (command == "exit" || command == "break") {
-//                break;
-            } else {
-                window.console.log("That is not a valid command");
-            }
-        }
-    }
-//    window.console.log("program terminated");
-//}
-//main();
+//         if (command !== null) {
+//             if (command == "show") {
+//                 showEmployees(employeeList);
+//             } else if (command == "add") {
+//                 addEmployees(employeeList);
+//             } else if (command == "del") {
+//                 deleteEmployees(employeeList);
+// //            } else if (command == "exit" || command == "break") {
+// //                break;
+//             } else {
+//                 window.console.log("That is not a valid command");
+//             }
+//         }
+//     }
+// //    window.console.log("program terminated");
+// //}
+// //main();
